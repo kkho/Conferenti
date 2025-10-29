595a1053adc8a658ec9f0c89e7bed7e58cf5845a
kkho <khiem_89@hotmail.com>
2025-10-29 12:31:50 +0100

    TASK-100: Fix cleanup for each test

--- conferenti-api/test/Conferenti.Domain.Tests.Unit/Sessions/SessionData.cs
+++ conferenti-api/test/Conferenti.Domain.Tests.Unit/Sessions/SessionData.cs
@@ -19,6 +19,7 @@ internal static class SessionData
     {
         Id = Id,
         Title = Title,
-        Description = Description
+        Description = Description,
+        SpeakerIds = []
     };
 }
